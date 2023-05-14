4b2b9f73de66b1ac106a019c9be79a8183c77945
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2023-05-14 20:03:05 -0300

    agregue el header

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/view/index.html");
+});
+
 app.listen(3000, () => {
   console.log("El servidor está funcionando en http://localhost:3000");
 });
